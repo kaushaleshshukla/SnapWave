c7c0c3d66a529282dd28f7cf1ae2bae1246bd462
Kaushalesh Shukla <kaushalesh.shukla@schrodinger.com>
2025-08-03 20:10:01 +0530

    Adding init file in crud dir.

---
+++ backend/app/crud/__init__.py
@@ -0,0 +1,17 @@
+# Import all crud modules and create convenience modules
+from app.crud import user
+
+# Create a "user" submodule that contains all user-related functions
+class UserCRUD:
+    from app.crud.user import (
+        get_user_by_email,
+        get_user_by_username,
+        get_user_by_id,
+        get_users,
+        create_user,
+        update_user,
+        authenticate_user
+    )
+
+# Export the user submodule
+user = UserCRUD
