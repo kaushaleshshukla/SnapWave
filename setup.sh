cede2d7ad2ef0cb04155b5320a39aca5dc1687b8
Shivam Verma <sv805388@gmail.com>
2025-08-09 22:28:32 +0530

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,20 +1,13 @@
 #!/bin/bash
 
 python -m venv venv
-case $(uname | tr '[:upper:]' '[:lower:]') in
-  linux*|darwin*)
-    # Script for Linux/macOS
-    source $PWD/venv/bin/activate
-    ;;
-  msys*|mingw*)
-    # Script for Windows (using Git Bash/Cygwin)
-    $PWD/venv/bin/activate.bat
-    ;;
-  *)
-    echo "Unknown OS, cannot execute script"
-    exit 1
-    ;;
-esac
+if [[ "$OSTYPE" == "msys" ]] || [[ "$OSTYPE" == "win32" ]] || [[ "$OSTYPE" == "cygwin" ]]; then
+    # Windows (Git Bash, MSYS, Cygwin)
+    source venv/Scripts/activate
+else
+    # Linux or macOS
+    source venv/bin/activate
+fi
 
 # pip install -r $PWD/backend/requirements.txt --no-dependencies
-pip install -r $PWD/backend/requirements.txt
+pip install -r backend/requirements.txt
